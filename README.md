988820be0079dc8378f4be70faefcbaf13bd906b
Matěj Půhoný <info@jetam.eu>
2020-09-21 21:17:55 +0200

    update

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 Tento projekt vzniká jako maturitní práce: Matěj Půhoný - 2020
 
-
+### Instalace na RPI lite
+1) nastavení wifi - sudo raspi-config
+2) instalace git `sudo apt install git-all`
+3) git clone https://github.com/JetamCZ/wifi.git
+4) cd wifi
 
 
---
+++ install-wifi.sh
@@ -0,0 +1,12 @@
+sudo apt update && sudo apt upgrade
+sudo apt install raspberrypi-kernel-headers aircrack-ng bc
+git clone https://github.com/aircrack-ng/rtl8188eus
+cd rtl8188eus
+sudo -i
+echo "blacklist r8188eu" > "/etc/modprobe.d/realtek.conf"
+exit
+make
+sudo make install
+cd ..
+rm -rf rtl8188eus
+echo "Please Reboot"
