96881dc1c45594bd979bd0f552d3b47f73bf01b8
Matěj Půhoný <info@jetam.eu>
2021-01-10 22:58:01 +0100

    Scapy testing

---
+++ scapy-test/sniff.py
@@ -0,0 +1,20 @@
+import os
+from scapy.all import *
+from dot11_frame import Dot11Frame
+
+def doData(pkt): 
+    try: 
+        print(pkt.summary)
+    except Exception as e:
+        print(e)
+
+def main():
+    os.system("ifconfig wlan1 down")
+    os.system("iwconfig wlan1 mode monitor")
+    os.system("ifconfig wlan1 up")
+
+    sniff(iface="wlan1", prn=doData)
+
+
+if __name__ == '__main__':
+    main()
