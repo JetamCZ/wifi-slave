3c974e9c6b4cfdade514bc31ef416c7617f7747e
Matěj Půhoný <info@jetam.eu>
2021-02-22 14:05:11 +0100

    update scapy

--- scapy-test/sniff.py
+++ scapy-test/sniff.py
@@ -1,14 +1,43 @@
 import os
 from scapy.all import *
 from dot11_frame import Dot11Frame
+import requests
+
+def getserial():
+  # Extract serial from cpuinfo file
+  cpuserial = "0000000000000000"
+  try:
+    f = open('/proc/cpuinfo','r')
+    for line in f:
+      if line[0:6]=='Serial':
+        cpuserial = line[10:26]
+    f.close()
+  except:
+    cpuserial = "ERROR000000000"
+ 
+  return cpuserial
 
 def doData(pkt): 
     try: 
         if(pkt.haslayer(Dot11)):
             frame = Dot11Frame(pkt, iface="wlan1")
 
-            if(frame.src == "e0:d0:83:d6:2a:57" or frame.dst == "e0:d0:83:d6:2a:57"):
-                print(frame.src, frame.dst, frame.signal_strength, frame.ssid)
+            print(frame.src, frame.dst, frame.signal_strength, frame.ssid)
+
+            new_data = {
+                'devices': [
+                    {
+                        rssi: frame.signal_strength,
+                        mac: frame.src
+                    }
+                ],
+                'device_key': getserial()
+            }
+
+            data = json.dumps(new_data)
+            headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
+
+            requests.post(url= "https://api-wifi.puhony.eu/data", data = data, headers = headers)
         
     except Exception as e:
         print(e)
